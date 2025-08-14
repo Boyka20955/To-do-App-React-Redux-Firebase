06720c24da9da7f96af88fa8066d040825160a60
Francis Irungu <149059599+Boyka20955@users.noreply.github.com>
2025-08-14 14:45:48 +0300

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,12 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDI99lOqIjDCs1A3Zq4W-Ey_nN067WxhXc",
-  authDomain: "to-do-app-d474e.firebaseapp.com",
-  projectId: "to-do-app-d474e",
-  storageBucket: "to-do-app-d474e.firebasestorage.app",
-  messagingSenderId: "849874683464",
-  appId: "1:849874683464:web:410f486f75f6e479087a7d"
+    apiKey: "Use your firebase ApiKey",
+  authDomain: "Use your firebase Auth Domain",
+  projectId: "Use your firebase Project ID",
+  storageBucket: "Use your firebase Storage Bucket",
+  messagingSenderId: "Use your firebase Messaging Sender ID",
+  appId: "Use your firebase App ID",
+  measurementId: "Use your firebase Measurement ID",
 };
 
 firebase.initializeApp(firebaseConfig);
